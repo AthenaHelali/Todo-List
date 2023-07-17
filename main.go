df1a44789565d1a760ce3072423a995a80bd5a6b
athena <athenaa.h93@gmail.com>
2023-07-17 15:41:54 +0330

    category entity and it's validation added

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type User struct {
@@ -15,18 +16,25 @@ type User struct {
 }
 
 type Task struct {
-	ID       int
-	Title    string
-	DueDate  string
-	Category string
-	IsDone   bool
-	UserID   int
+	ID         int
+	Title      string
+	DueDate    string
+	CategoryID int
+	IsDone     bool
+	UserID     int
+}
+type Category struct {
+	ID     int
+	Title  string
+	Color  string
+	UserID int
 }
 
 var userStorage []User
 var authenticatedUser *User
 
 var taskStorage []Task
+var categoryStorage []Category
 
 func main() {
 	fmt.Println("Hello to TODO app")
@@ -76,20 +84,37 @@ func createTask() {
 	scanner.Scan()
 	title = scanner.Text()
 
-	fmt.Println("please enter the task category")
+	fmt.Println("please enter the task category-id")
 	scanner.Scan()
 	category = scanner.Text()
+	categoryID, err := strconv.Atoi(category)
+	if err != nil {
+		fmt.Printf("category-id is not valid, %v\n", err)
+
+		return
+	}
+	isFound := false
+	for _, c := range categoryStorage {
+		if c.ID == categoryID && c.UserID == authenticatedUser.ID {
+			isFound = true
+			break
+		}
+	}
+	if !isFound {
+		fmt.Printf("category-id is not found\n")
+		return
+	}
 
 	fmt.Println("please enter the task due date")
 	scanner.Scan()
 	duedate = scanner.Text()
 	task := Task{
-		ID:       len(taskStorage) + 1,
-		Title:    title,
-		DueDate:  duedate,
-		Category: category,
-		IsDone:   false,
-		UserID:   authenticatedUser.ID,
+		ID:         len(taskStorage) + 1,
+		Title:      title,
+		DueDate:    duedate,
+		CategoryID: categoryID,
+		IsDone:     false,
+		UserID:     authenticatedUser.ID,
 	}
 	taskStorage = append(taskStorage, task)
 
@@ -107,6 +132,14 @@ func createCategory() {
 	color = scanner.Text()
 
 	fmt.Println("category:", title, color)
+
+	c := Category{
+		ID:     len(categoryStorage) + 1,
+		Title:  title,
+		Color:  color,
+		UserID: authenticatedUser.ID,
+	}
+	categoryStorage = append(categoryStorage, c)
 
 }
 func registerUser() {
@@ -127,13 +160,13 @@ func registerUser() {
 
 	fmt.Printf("user:%v %v %v %v\n", len(userStorage)+1, email, password, name)
 
-	user := User{
+	u := User{
 		ID:       len(userStorage) + 1,
 		Name:     name,
 		Email:    email,
 		Password: password,
 	}
-	userStorage = append(userStorage, user)
+	userStorage = append(userStorage, u)
 
 }
 func listTask() {
